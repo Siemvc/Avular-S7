4923d335060b870f066f84c93edc62d1db86784d
Teun <teunteun.12@gmail.com>
2026-01-08 10:45:09 +0100

    Nieuwe Micro-ROS implementatie

--- RealTimeController/src/main.cpp
+++ RealTimeController/src/main.cpp
@@ -4,20 +4,21 @@
 #include <rcl/rcl.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
-#include <std_msgs/msg/string.h>
 
-// Globale variabelen voor micro-ROS
+#include <std_msgs/msg/int32.h>
+
 rcl_publisher_t publisher;
-std_msgs__msg__String msg;
+std_msgs__msg__Int32 msg;
+rclc_executor_t executor;
 rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t node;
+rcl_timer_t timer;
 
-// Macro om errors te checken
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 
-// Als er iets fout gaat, knippert de ingebouwde LED snel
+// Error blinky loop
 void error_loop(){
   while(1){
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
@@ -25,45 +26,55 @@ void error_loop(){
   }
 }
 
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+{
+  RCLC_UNUSED(last_call_time);
+  if (timer != NULL) {
+    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
+    msg.data++;
+  }
+}
+
 void setup() {
-  // Zet de transport layer op Serial (USB)
+  // Gebruik de USB Serial verbinding
   Serial.begin(115200);
   set_microros_serial_transports(Serial);
-  
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);  
 
-  delay(2000); // Geef de Agent even tijd om te verbinden
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  delay(2000);
 
   allocator = rcl_get_default_allocator();
 
-  // 1. Initialiseer support
+  // Initialiseer micro-ROS
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
-  // 2. Maak een Node aan genaamd "teensy_node"
+  // Maak node 'teensy_node'
   RCCHECK(rclc_node_init_default(&node, "teensy_node", "", &support));
 
-  // 3. Maak een Publisher aan op topic "teensy_text"
+  // Maak publisher op topic 'micro_ros_teensy_count'
   RCCHECK(rclc_publisher_init_default(
     &publisher,
     &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String),
-    "teensy_text"));
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "micro_ros_teensy_count"));
 
-  // Initialiseer het bericht
-  msg.data.data = (char * ) malloc(100 * sizeof(char));
-  msg.data.size = 0;
-  msg.data.capacity = 100;
+  // Maak timer (publiceer elke 1000ms)
+  RCCHECK(rclc_timer_init_default(
+    &timer,
+    &support,
+    RCL_MS_TO_NS(1000),
+    timer_callback));
+
+  // Executor setup
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+
+  msg.data = 0;
 }
 
 void loop() {
-  // Vul het bericht
-  sprintf(msg.data.data, "Actuator status: OK - Tijd: %lu", millis());
-  msg.data.size = strlen(msg.data.data);
-
-  // Verstuur het bericht
-  RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-
-  // Even wachten
-  delay(1000);
+  delay(100);
+  RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
 }
