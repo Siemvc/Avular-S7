a326fd53595c0fda1f2e4f09195cf6bcf6b21885
freeky-01 <freekvankasteren@gmail.com>
2026-01-15 12:24:59 +0100

    Controller uitlezen via teensy werkt!

--- RealTimeController/src/main.cpp
+++ RealTimeController/src/main.cpp
@@ -1,111 +1,128 @@
 #include <Arduino.h>
-#include <micro_ros_platformio.h>
-#include <DHT.h> // De sensor library
+#include <micro_ros_arduino.h>
 
+#include <stdio.h>
 #include <rcl/rcl.h>
+#include <rcl/error_handling.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 
-// We gebruiken Float32 voor decimalen
-#include <std_msgs/msg/float32.h>
+#include <geometry_msgs/msg/twist.h>
+#include <std_msgs/msg/bool.h>
+#include <std_msgs/msg/string.h>
 
-// Sensor instellingen
-const int DHTPIN = 16;
-#define DHTTYPE DHT11
-DHT dht(DHTPIN, DHTTYPE);
-
-// Micro-ROS variabelen
-rcl_publisher_t temp_publisher;
-rcl_publisher_t hum_publisher;
-std_msgs__msg__Float32 temp_msg;
-std_msgs__msg__Float32 hum_msg;
-// Executor en support structuren
-rclc_executor_t executor;
+// --- MICRO-ROS OBJECTEN ---
+rcl_node_t node;
 rclc_support_t support;
 rcl_allocator_t allocator;
-rcl_node_t node;
-rcl_timer_t timer;
+rclc_executor_t executor;
 
-// Error check macro's
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
-#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+// Publishers & Subscribers
+rcl_publisher_t debug_pub; // NIEUW: Om tekst terug te sturen naar de PC
+rcl_subscription_t sub_actuator;
+rcl_subscription_t sub_lights;
+rcl_subscription_t sub_standby;
+rcl_subscription_t sub_drive_mode;
 
-void error_loop(){
-  while(1){
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    delay(100);
-  }
+// Messages
+geometry_msgs__msg__Twist msg_twist;
+std_msgs__msg__Bool msg_lights;
+std_msgs__msg__Bool msg_standby;
+std_msgs__msg__String msg_drive_mode;
+std_msgs__msg__String msg_debug; // Het bericht dat we terugsturen
+
+// Buffer voor de debug tekst
+char debug_buffer[200]; 
+// Buffer voor inkomende string (drive mode)
+char drive_mode_buffer[50]; 
+
+// Hulpfunctie om te publishen
+void publish_debug(const char* text) {
+  // Vul het message object
+  msg_debug.data.data = (char*)text;
+  msg_debug.data.size = strlen(text);
+  msg_debug.data.capacity = strlen(text) + 1;
+
+  // Stuur naar PC
+  rcl_publish(&debug_pub, &msg_debug, NULL);
 }
 
-// Deze functie wordt elke seconde aangeroepen
-void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
-{
-  RCLC_UNUSED(last_call_time);
-  if (timer != NULL) {
-    // 1. Lees de sensor
-    float humidity = dht.readHumidity();
-    float temperature = dht.readTemperature();
+// --- CALLBACKS (Alleen printen naar ROS topic) ---
 
-    // Check of het lezen gelukt is (isnan = is not a number)
-    if (isnan(humidity) || isnan(temperature )) {
-        // Als het faalt, doen we even niks (of je kunt een error loggen)
-        return;
-    }
-
-    // 2. Vul de berichten
-    temp_msg.data = temperature;
-    hum_msg.data = humidity;
-
-    // 3. Publiceer de data
-    RCSOFTCHECK(rcl_publish(&temp_publisher, &temp_msg, NULL));
-    RCSOFTCHECK(rcl_publish(&hum_publisher, &hum_msg, NULL));
-  }
+void callback_actuator(const void * msgin) {
+  const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msgin;
+  
+  // Format een string met de waardes
+  sprintf(debug_buffer, "ACTUATOR | LinY (Gas): %.2f | AngZ (Stuur): %.2f | AngX (Lift): %.2f | AngY (Tilt): %.2f", 
+          msg->linear.y, msg->angular.z, msg->angular.x, msg->angular.y);
+          
+  publish_debug(debug_buffer);
 }
+
+void callback_lights(const void * msgin) {
+  const std_msgs__msg__Bool * msg = (const std_msgs__msg__Bool *)msgin;
+  
+  sprintf(debug_buffer, "LIGHTS | Status: %s", msg->data ? "AAN" : "UIT");
+  publish_debug(debug_buffer);
+}
+
+void callback_standby(const void * msgin) {
+  const std_msgs__msg__Bool * msg = (const std_msgs__msg__Bool *)msgin;
+  
+  sprintf(debug_buffer, "STANDBY | Status: %s", msg->data ? "ACTIEF" : "INACTIEF");
+  publish_debug(debug_buffer);
+}
+
+void callback_mode(const void * msgin) {
+  const std_msgs__msg__String * msg = (const std_msgs__msg__String *)msgin;
+  
+  sprintf(debug_buffer, "MODE | Nieuwe modus: %s", msg->data.data);
+  publish_debug(debug_buffer);
+}
+
+// --- SETUP & LOOP ---
 
 void setup() {
-  Serial.begin(115200);
-  set_microros_serial_transports(Serial);
+  set_microros_transports(); // Serial (USB) wordt gebruikt voor ROS
 
-  // Start de sensor
-  dht.begin();
-  
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH); 
   delay(2000);
 
   allocator = rcl_get_default_allocator();
 
-  // Init micro-ROS
-  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
-  RCCHECK(rclc_node_init_default(&node, "teensy_sensor_node", "", &support));
+  // Create Node
+  rclc_support_init(&support, 0, NULL, &allocator);
+  rclc_node_init_default(&node, "teensy_debug_node", "", &support);
 
-  // Maak Publisher voor Temperatuur
-  RCCHECK(rclc_publisher_init_default(
-    &temp_publisher,
+  // 1. Maak de Debug Publisher (Hier luister je naar op je PC)
+  rclc_publisher_init_default(
+    &debug_pub,
     &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
-    "sensor/temperature"));
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String),
+    "/teensy_debug"
+  );
 
-  // Maak Publisher voor Luchtvochtigheid
-  RCCHECK(rclc_publisher_init_default(
-    &hum_publisher,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
-    "sensor/humidity"));
+  // 2. Maak Subscribers (Zelfde topics als Python code)
+  rclc_subscription_init_default(&sub_actuator, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist), "/actuator_pub");
+  rclc_subscription_init_default(&sub_lights, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Bool), "/lights_toggle");
+  rclc_subscription_init_default(&sub_standby, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Bool), "/standby");
+  rclc_subscription_init_default(&sub_drive_mode, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String), "/drive_mode");
 
-  // Timer: 1 seconde), dit is de sample rate van de sensor (ja hij is echt zo traag)
-  RCCHECK(rclc_timer_init_default(
-    &timer,
-    &support,
-    RCL_MS_TO_NS(1000),
-    timer_callback));
+  // Geheugen reserveren voor inkomende strings
+  msg_drive_mode.data.data = drive_mode_buffer;
+  msg_drive_mode.data.capacity = sizeof(drive_mode_buffer);
 
-  // Executor (1 handle is genoeg voor alleen de timer)
-  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
-  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+  // Executor
+  rclc_executor_init(&executor, &support.context, 4, &allocator);
+  rclc_executor_add_subscription(&executor, &sub_actuator, &msg_twist, &callback_actuator, ON_NEW_DATA);
+  rclc_executor_add_subscription(&executor, &sub_lights, &msg_lights, &callback_lights, ON_NEW_DATA);
+  rclc_executor_add_subscription(&executor, &sub_standby, &msg_standby, &callback_standby, ON_NEW_DATA);
+  rclc_executor_add_subscription(&executor, &sub_drive_mode, &msg_drive_mode, &callback_mode, ON_NEW_DATA);
+  
+  // Stuur een startberichtje
+  publish_debug("Teensy Debugger Started. Waiting for data...");
 }
 
 void loop() {
-  RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
-  delay(10);
+  rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+  // Geen motor updates nodig nu
 }
