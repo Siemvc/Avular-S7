6c1f6e78ae2f6258082e2658bc2d9f9b266b8f1d
freeky-01 <46967100+freeky-01@users.noreply.github.com>
2026-01-27 15:27:48 +0100

    forgot to save files before push

---
+++ RealTimeController/include/LedManager.h
@@ -0,0 +1,38 @@
+#ifndef LED_MANAGER_H
+#define LED_MANAGER_H
+
+#include <FastLED.h>
+
+enum avuloaderState {
+  Startup,
+  Shutdown,
+  Standby,
+  Operational,
+  Driving,
+  E_Brake,
+  Low_power,
+  ERR,
+  Linux_boot_ERR,
+  IDLE
+};
+
+class LedManager {
+public:
+    LedManager() : _numLeds(0), _leds(nullptr) {}
+    
+    void begin(int pin, int numLeds);
+    void update();
+    void setState(avuloaderState newState);
+
+private:
+    int _numLeds;
+    CRGB* _leds; 
+    avuloaderState _currentMode = Startup;
+    unsigned long _startTime;
+    void handleLeds();
+    void effectBreathing(CRGB color);
+    void effectFlash(CRGB color, int interval);
+    void solidEverywhere(CRGB color);
+};
+
+#endif
---
+++ RealTimeController/src/LedManager.cpp
@@ -0,0 +1,55 @@
+#include "LedManager.h"
+
+void LedManager::begin(int pin, int numLeds) {
+    _numLeds = numLeds;
+    _leds = new CRGB[_numLeds];
+
+    FastLED.addLeds<WS2812, 23, GRB>(_leds, _numLeds); 
+    
+    FastLED.setBrightness(50);
+
+    _currentMode = Startup;
+    _startTime = millis();
+}
+
+void LedManager::update() {
+    handleLeds();
+    FastLED.show();
+}
+
+void LedManager::handleLeds() {
+    switch (_currentMode) {
+        case Startup:
+            if(millis()- _startTime < 5000){
+                effectFlash(CRGB::Green2, 300)
+            } else{
+                _currentMode = Standby;
+            }
+        case Shutdown:       effectBreathing(CRGB::Magenta3); break;
+        case Standby:        effectBreathing(CRGB::Blue3); break;
+        case Operational:    effectBreathing(CRGB::Green2); break;
+        case Driving:        solidEverywhere(CRGB::Green2); break;
+        case E_Brake:        solidEverywhere(CRGB::Red2); break;
+        case Low_power:      solidEverywhere(CRGB::Yellow1); break;
+        case ERR:            effectBreathing(CRGB::Yellow1); break;
+        case Linux_boot_ERR: effectFlash(CRGB::Yellow1, 200); break;
+        default:             effectBreathing(CRGB::Amethyst); break;
+    }
+}
+
+void LedManager::effectBreathing(CRGB color) {
+    float breath = (exp(sin(millis()/2000.0*PI)) - 0.36787944)*108.0;
+    for(int i=0; i < _numLeds; i++) {
+        _leds[i] = color;
+        _leds[i].fadeToBlackBy(255 - breath);
+    }
+}
+
+void LedManager::effectFlash(CRGB color, int interval) {
+    CRGB current = (millis() / interval) % 2 == 0 ? color : CRGB::Black;
+    fill_solid(_leds, _numLeds, current);
+}
+
+void LedManager::solidEverywhere(CRGB color) {
+    fill_solid(_leds, _numLeds, color);
+}
