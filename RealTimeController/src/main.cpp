6ec0b5c364fb31e0058824cc014f00c65ace23c7
Siemvc <144440423+Siemvc@users.noreply.github.com>
2026-02-02 14:00:56 +0100

    battery indication fix replaced switch with if statements

--- RealTimeController/src/main.cpp
+++ RealTimeController/src/main.cpp
@@ -22,6 +22,8 @@ BMS bms[2] = { BMS(BMS_ADDR_LIST[0]), BMS(BMS_ADDR_LIST[1]) };
 
 float batteryVoltage = 0.0f;
 const float batteryLowVoltageThreshold = 19.0; // Voltage threshold for low battery indication
+const float batteryCriticalVoltageThreshold = 18.35; // Voltage threshold for critical battery indication
+const float batteryDeadVoltageThreshold = 18.1; // Voltage threshold for dead battery indication
 
 const float overheatTemperatureThreshold = 60.0; // Temperature threshold for overheating indication
 
@@ -289,22 +291,13 @@ void loop() {
             batteryVoltage += p.totalVoltage_V / 2.0f; // Average voltage from both BMS units
         }
 
-        // switch (batteryVoltage){
-        //   case batteryLowVoltageThreshold - 0.7 ... batteryLowVoltageThreshold + 0.2:
-        //     leds.setState(Low_power);
-        //     break;
-
-        //   case batteryLowVoltageThreshold - 0.85 ... batteryLowVoltageThreshold - 0.7:
-        //     leds.setState(battery_empty);
-        //     break;
-
-        //   case .. batteryLowVoltageThreshold - 0.85:
-        //     leds.setState(battery_dead);
-        //     break;
-
-        //   default:
-        //     break; // This should do nothing
-        //   } 
+         if (batteryVoltage >= batteryCriticalVoltageThreshold && batteryVoltage <= batteryLowVoltageThreshold) {
+             leds.setState(Low_power);
+         } else if (batteryVoltage >= batteryDeadVoltageThreshold && batteryVoltage < batteryCriticalVoltageThreshold) {
+             leds.setState(battery_empty);
+         } else if (batteryVoltage < batteryDeadVoltageThreshold) {
+             leds.setState(battery_dead);
+         } 
           lastCanBusBMSRead = millis();
     }
   //FIXME: Re-enable moving LED state when initial led testing of setup is done
