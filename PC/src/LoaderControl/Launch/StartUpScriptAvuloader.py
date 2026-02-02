e03c3c1c0c3a7f57cde108653701d8f7c25cd1ba
freeky-01 <freekvankasteren@gmail.com>
2026-02-02 11:56:15 +0100

    Bestandslocatie startupscript

---
+++ PC/src/LoaderControl/Launch/StartUpScriptAvuloader.py
@@ -0,0 +1,28 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+
+def generate_launch_description():
+    return LaunchDescription([
+        # 1. Start de Micro-ROS Agent
+        ExecuteProcess(
+            cmd=['ros2', 'run', 'micro_ros_agent', 'micro_ros_agent', 'serial', '--dev', '/dev/ttyACM0'],
+            output='screen'
+        ),
+        
+        # 2. Start de PS4 Joy Node
+        Node(
+            package='joy',
+            executable='joy_node',
+            name='joy_node',
+            output='screen'
+        ),
+
+        # 3. Start jouw Control Node
+        Node(
+            package='control_turtle',
+            executable='control_Node',
+            name='loader_controller',
+            output='screen'
+        )
+    ])
